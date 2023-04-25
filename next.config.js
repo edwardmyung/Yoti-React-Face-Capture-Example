cb69a29a61d62c1544ebeaa79abd5cf6534437f5
Edward Myung <edward.myung@juul.com>
2023-04-25 14:50:51 +0100

    React face capture working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, './node_modules/@getyoti/react-face-capture/assets'),
+            to: path.resolve(__dirname, './public/yoti-assets'),
+          },
+        ],
+      }),
+    )
+
+    return config
+  }
 }
 
 module.exports = nextConfig
